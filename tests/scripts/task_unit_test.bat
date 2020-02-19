88a7a8fc16caf881c75f937930c22ca26d871af2
Minjie Wang <wmjlyjemaine@gmail.com>
2020-02-19 14:41:55 +0000

    another way

--- tests/scripts/task_unit_test.bat
+++ tests/scripts/task_unit_test.bat
@@ -12,7 +12,7 @@ CALL workon %BUILD_TAG%
 SET PYTHONPATH=!CD!\python;!PYTHONPATH!
 SET DGLBACKEND=!BACKEND!
 SET DGL_DOWNLOAD_DIR=!CD!
-
+dir _deps
 python -m pip install _deps\dgl*.whl --user || EXIT /B 1
 python -m pip install pytest --user || EXIT /B 1
 python -m pytest -v --junitxml=pytest_unit.xml tests\unittests || EXIT /B 1
